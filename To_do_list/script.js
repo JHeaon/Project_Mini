6c7a0a8fae276161256d90de05f46e0a8e046c22
정해원 <j3heawon@naver.com>
2022-05-08 04:13:28 +0900

    To_do_list

--- To_do_list/script.js
+++ To_do_list/script.js
@@ -1,14 +1,14 @@
 let inputBox = document.getElementById('inputField');  
 let addToDo = document.getElementById('addToDo');      
-let toDoList = document.getElementById('toDoList');    
+let toDoList = document.getElementById('toDoList');   
 
 addToDo.addEventListener('click', function(){    
-    var list = document.createElement('li');     
-    if (!inputBox.value)            
+    var list = document.createElement('li');    
+    if (!inputBox.value)           
         alert('내용을 입력해 주세요!');
     else
     {
-        list.innerText = inputBox.value;  
+        list.innerText = inputBox.value; 
         toDoList.appendChild(list);       
         inputBox.value= "";               
     }
@@ -16,7 +16,7 @@ addToDo.addEventListener('click', function(){
     list.addEventListener('click', function(){      
         list.style.textDecoration = "line-through";
     })
-    list.addEventListener('dblclick', function(){   
+    list.addEventListener('dblclick', function(){  
         toDoList.removeChild(list);
     })
 })
