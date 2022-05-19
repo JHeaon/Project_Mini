0496e137ec86bf3ad25466974bcfbc1e89307a3d
정해원 <j3heawon@naver.com>
2022-05-19 14:00:23 +0900

    update_22_05_19

--- Img_crawling/img_crawling.py
+++ Img_crawling/img_crawling.py
@@ -4,10 +4,16 @@ import urllib.request
 from urllib.parse import quote
 from selenium import webdriver
 
+# 403 에러 방지
+opener = urllib.request.build_opener()
+opener.addheaders = [
+    ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+urllib.request.install_opener(opener)
+
 
 word = input("검색할 단어 : ")
 url = f"https://www.google.com/search?q={quote(word)}&tbm=isch&ved="
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("Img_crawling\chromedriver.exe")
 driver.get(url)
 time.sleep(1)
 
@@ -25,13 +31,3 @@ for tag in tags:
     urllib.request.urlretrieve(img, f"{word}//" + f"{word}{cnt}.jpg")
     cnt += 1
     time.sleep(2)
-
-
-
-
-
-
-
-
-
-
